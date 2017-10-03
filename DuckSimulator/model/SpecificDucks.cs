863cde054e205f02ff3f69918ef5c5c4b4003e17
Matteo Venturi <matteo.venturi7@gmail.com>
2017-10-03 14:20:08 +0200

    Refactor specific ducks

--- DuckSimulator/model/SpecificDucks.cs
+++ DuckSimulator/model/SpecificDucks.cs
@@ -22,3 +22,50 @@ public class MallardDuck : Duck, IFlayable, IQuackable, ISwimable
         totSwimKM += meters;
     }
 }
+
+public class ReadHeadDuck : Duck, IFlayable, IQuackable, ISwimable
+{
+    private int totFlyKM, totSwimKM;
+    public override string Name => "Red head duck";
+
+    public int TotalFlyKM => totFlyKM;
+
+    public int TotalSwimKM => totSwimKM;
+
+    public void Fly(int meters)
+    {
+        totFlyKM += meters;
+    }
+
+    public string Quack()
+    {
+        return "Quack";
+    }
+
+    public void Swim(int meters)
+    {
+        totSwimKM += meters;
+    }
+}
+
+public class RubberDuck : Duck, IQuackable, ISwimable
+{
+    private int totSwimKM;
+    public override string Name => "Rubber duck";
+
+    public int TotalSwimKM => totSwimKM;
+
+    public string Quack()
+    {
+        return "Squack";
+    }
+
+    public void Swim(int meters){
+        totSwimKM += 1;
+    }
+}
+
+public class DecoyDuck : Duck
+{
+    public override string Name => "Decoy duck";
+}
